556f0ed02d0fd57bdb7bf340d00ffced2334bd85
nikl <dl@lahaye-tiedemann.de>
2020-06-22 21:28:41 +0200

    base

---
+++ ltg_roma_kompendium_2006_E01.js
@@ -0,0 +1,20 @@
+﻿//ltg_roma_kompendium_2006_E01.jsx
+//
+//Vergibt bestehender XML-Struktur GUID und Time-Stamp.
+//
+
+main();
+
+function main(){
+	//Make certain that user interaction (display of dialogs, etc.) is turned on.
+	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.interactWithAll;
+	var myObjectList = new Array;
+	if(app.documents.length != 0){
+		tellme();
+
+	}
+	
+}
+
+function display(mCon){
+}
