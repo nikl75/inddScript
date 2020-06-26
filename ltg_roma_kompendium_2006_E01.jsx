61bf3895948d398675f6d916185ebcfb75bccd87
nikl <dl@lahaye-tiedemann.de>
2020-06-26 08:07:17 +0200

    v0.1 first running with random and timestamp

--- ltg_roma_kompendium_2006_E01.jsx
+++ ltg_roma_kompendium_2006_E01.jsx
@@ -3,19 +3,20 @@
 //Vergibt bestehender XML-Struktur GUID und Time-Stamp.
 //
 
+var tTime = new Date();
+
 main();
 
 function main() {
 	var dok = app.activeDocument;
 	var rootXML = dok.xmlElements[0];
+	var tElementCount = 0;
 
-
-	var tTags = dok.xmlTags;
 
 	var tItems = rootXML.evaluateXPathExpression("//*");
 	// var tItems = rootXML.evaluateXPathExpression("//B02_Abschnitt");
 
-	pop(tItems.length);
+	// pop(tItems.length);
 
 	for (var i = 0; i < tItems.length; i++) {
 		var tItem = tItems[i];
@@ -24,8 +25,10 @@ function main() {
 			var tGUID = buildGUID();
 			tItem.xmlAttributes.add("GUID", tGUID);
 			// pop(tItem.isValid);
+			tElementCount++;
 		}
 	}
+	pop(tElementCount + " Elemente geändert");
 }
 
 function pop(mCon) {
@@ -33,5 +36,15 @@ function pop(mCon) {
 }
 
 function buildGUID(){
-		return "GUID_XXX_123456";
+	/* in die GUID noch teile des Dateinamen einfügen
+	 * dient der Versionierung und das GUID dokument-übergreifend nicht 
+	 * doppelt vergeben werden können 
+	 */
+	 var tGUID = "GUID_roma_kompendium_" + "DATEINAME" + "_" +  + tTime.getTime() + "_" + getRandomInt(100000);
+
+		return tGUID;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
